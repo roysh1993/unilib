0938b300a9d6fadd1a37191fa59518069a9e8c27
royshafir <royshafir8@gmail.com>
2020-08-06 19:22:25 +0300

    modified for command line use

--- scripts.py
+++ scripts.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO, Seq
+import sys
 import json
-from scipy.io import loadmat
+# from scipy.io import loadmat
 
 forward_primer = "CTTACACTGACAACACTCGTC"
 reversed_primer = "GACGAGTGTTGTCAGTGTAAG"
@@ -32,9 +33,9 @@ def check_reverse_primer(read):
     return result
 
 
-def covert_to_forward():
-    fastq_input = "data/Sample_MRG/MRG_CTTGTA_mergred.fastq"
-    with open(fastq_input, "rU") as handle, open("data/MRG_CTTGTA_forward_strand.fastq", "w") as output_handle:
+def covert_to_forward(fastq_input,fastq_output):
+
+    with open(fastq_input, "rU") as handle, open(fastq_output, "w") as output_handle:
 
         for record in SeqIO.parse(handle, "fastq"):
             single_read = str(record.format("fastq")).split("\n")
@@ -49,40 +50,40 @@ def covert_to_forward():
                 SeqIO.write(record,output_handle,"fastq")
 
 
-def load_mat_to_json(filename):
-    f_name = "data/UNILIB_post_E_coli_plasmid/ListStrcut60000.mat"
-    data = loadmat(f_name)
-    # j_s = json.dumps(data)
-    data = data["List"]
-    data2 = data[0][0][0]
-
-    a= 5
-
-def try_func():
-    f_name = "data/UNILIB_post_E_coli_plasmid/ListStrcut60000.mat"
-    matdata = loadmat(f_name)
-    # First, we'll combine the data sets for each unique combination of instruments, station, and depth.
-    matdata2 = {}  # Creates an empty dictionary
-
-    for k, v in matdata.iteritems():  # Here's way to loop over key,value pairs of dictionaries.
-        if k.startswith('__'):  # Skip the special keys
-            continue  # Continue goes to the next loop iteration.
-        param, station, date, depth = k.split(
-            '_')  # This splits up the string, assigning each of the parts to different variables.
-
-        # The if statements here just check to make sure that level of the dictionary has already been created.
-        # If all levels are present, we can just assign a new key.  If not, we have to create a new dictionary
-        # at that level.
-        if station in matdata2:
-            if param in matdata2[station]:
-                if depth in matdata2[station][param]:
-                    matdata2[station][param][depth][date] = v  # We don't need to check depth b/c we're assigning it.
-                else:
-                    matdata2[station][param][depth] = {date: v}
-            else:
-                matdata2[station][param] = {depth: {date: v}}
-        else:
-            matdata2[station] = {param: {depth: {date: v}}}
+# def load_mat_to_json(filename):
+#     f_name = "data/UNILIB_post_E_coli_plasmid/ListStrcut60000.mat"
+#     data = loadmat(f_name)
+#     # j_s = json.dumps(data)
+#     data = data["List"]
+#     data2 = data[0][0][0]
+#
+#     a= 5
+#
+# def try_func():
+#     f_name = "data/UNILIB_post_E_coli_plasmid/ListStrcut60000.mat"
+#     matdata = loadmat(f_name)
+#     # First, we'll combine the data sets for each unique combination of instruments, station, and depth.
+#     matdata2 = {}  # Creates an empty dictionary
+#
+#     for k, v in matdata.iteritems():  # Here's way to loop over key,value pairs of dictionaries.
+#         if k.startswith('__'):  # Skip the special keys
+#             continue  # Continue goes to the next loop iteration.
+#         param, station, date, depth = k.split(
+#             '_')  # This splits up the string, assigning each of the parts to different variables.
+#
+#         # The if statements here just check to make sure that level of the dictionary has already been created.
+#         # If all levels are present, we can just assign a new key.  If not, we have to create a new dictionary
+#         # at that level.
+#         if station in matdata2:
+#             if param in matdata2[station]:
+#                 if depth in matdata2[station][param]:
+#                     matdata2[station][param][depth][date] = v  # We don't need to check depth b/c we're assigning it.
+#                 else:
+#                     matdata2[station][param][depth] = {date: v}
+#             else:
+#                 matdata2[station][param] = {depth: {date: v}}
+#         else:
+#             matdata2[station] = {param: {depth: {date: v}}}
 
 if __name__ == '__main__':
-    load_mat_to_json("Dasdsa")
+    covert_to_forward(sys.argv[1],sys.argv[2])
